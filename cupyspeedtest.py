249358f3157e31a2f2c4528a09338f58995314f8
Emily Thomas <alliancerune@gmail.com>
2023-08-28 11:46:50 -0600

    Add files via upload

--- cupyspeedtest.py
+++ cupyspeedtest.py
@@ -4,26 +4,30 @@ import time
 
 # speed test
 # qty 2 prefilled array size [12k, 12k] multiplying into the first,
-# 20 iterations: system is cheating, as if I printout the value of a single cell in a_arr, it *conveniently* takes 4x longer
-# using the "realistic" values: 270 270 270 280 280, these still seem a bit cheaty, and I have no confidence it is reporting an accurate number, so no score
+# 20 iterations: nothing outside of the 0th grid/thread will actually calc...
+# see http://users.wfu.edu/choss/CUDA/docs/Lecture%205.pdf
+# but something in CuPy is limiting me from allocating anywhere lcose to the maximum number of block*threads that I need
+# I seem to be limited to 20 blocks of 20 threads in x, and y, which is not a lot of data....
 
-Y_SIZE = 12000
-X_SIZE = 12000
-ITERATIONS = 20
+Y_SIZE = 3
+X_SIZE = 6
+ITERATIONS = 1
 
 
 def process(a, b):
-    return gp.multiply(a, b)
+    return gp.multiply(a, b) # this works, but wanted to see other ways to write kernels
 
 
 process_kernel = gp.RawKernel(r'''
 extern "C" __global__
-void my_mult(const double* a, const double* b, double* c) {
-    int tid = blockDim.x * blockIdx.x + threadIdx.x;
-    c[tid] = a[tid] * b[tid];
+void my_mult(const double* a, const double* b, double* c, int xWidth) {
+    int x = (blockDim.x * blockIdx.x) + threadIdx.x;
+    int y = (blockDim.y * blockIdx.y) + threadIdx.y;
+    int f = (y * xWidth) + x;
+    //c[f] = a[f] * b[f];
+    c[f] = f;
 }
-''', 'my_mult') # this does work if you call process_kernel((y,), (y,), (a_arr, b_arr, a_arr)) where y is 1, 500, 1000
-# but if you are going to write C kernels, just switch to C++ fam
+''', 'my_mult') # if you are going to write C kernels, just switch to C++ fam
 
 
 element_process_kernel = gp.ElementwiseKernel(
@@ -31,25 +35,32 @@ element_process_kernel = gp.ElementwiseKernel(
     'float64 c',
     'c = a * b',
     'element_process_kernel'
-)
+) # this has less control over a lot of dimensions
 
 
 # create 2d array from random
 a_arr = gp.random.uniform(0, 2, (Y_SIZE, X_SIZE), dtype=np.float64)
 b_arr = gp.random.uniform(0, 2, (Y_SIZE, X_SIZE), dtype=np.float64)
+c_arr = a_arr
+print(a_arr)
+print(b_arr)
 print("done prefilling")
+
+#
+tpb = (2, 2) # threads per block
+bpg = (2, 2) # blocks per grid, these are set to max number I can have them at
 
 # start timer and exec
 start = time.perf_counter()
 for iter in range(ITERATIONS):
     #print(iter)
-    #a_arr = np.multiply(a_arr, b_arr)
-    element_process_kernel(a_arr, b_arr, a_arr)
-    print("a ", a_arr[0][0]) # for some reason including this line here makes the results more "realistic", like it cheats if you don't witness some results
+    process_kernel(bpg, tpb, (a_arr, b_arr, c_arr, X_SIZE)) #(y,), (x,)
+    #element_process_kernel(a_arr, b_arr, a_arr)
+    a_arr = c_arr
+    print(c_arr)
     
 # stop timer
 end = time.perf_counter()
-print("{:.3f}".format(end - start), "secs")
+print("{:.2f}".format((end - start) * 1000), "ms")
 
-# kernel is for sure cheating, so make it do extra work after the test
-a_arr = element_process_kernel(a_arr, b_arr)
+print(c_arr)
